529799b7688f4becfa6b320ea1b88896be48e1f0
Morgan Sherrill <mo.sherrill@outlook.com>
2021-11-03 14:52:40 -0700

    added some work content area - added a hover effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,30 @@ $(document).ready(function(){
 jQuery(document).ready(function() {
 
   // select the item(s) to move randomly
-  var $randItem = $('#heroRow').find('.randomLeaf');
+  var $randItem = $('#heroCol1').find('.randomLeaf');
   
   // select the target for random placement
-  var $items = $('#heroRow').find('.randomLeaf');
+  var $items = $('#heroCol1').find('.randomLeaf');
+  
+  $($randItem).each(function() { 
+  var pos = Math.floor(Math.random() * $items.length)
+  $(this).insertAfter($items.eq(pos));
+  });
+
+  $randItem = $('#heroCol2').find('.randomLeaf');
+  
+  // select the target for random placement
+  $items = $('#heroCol2').find('.randomLeaf');
+  
+  $($randItem).each(function() { 
+  var pos = Math.floor(Math.random() * $items.length)
+  $(this).insertAfter($items.eq(pos));
+  });
+
+  $randItem = $(document).find('.randomLeaf');
+  
+  // select the target for random placement
+  $items = $(document).find(".row");
   
   $($randItem).each(function() { 
   var pos = Math.floor(Math.random() * $items.length)
